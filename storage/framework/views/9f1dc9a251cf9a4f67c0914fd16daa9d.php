f3c900010176faf7304e662c90c37ad4c210e456
Arun Agnes <128603782+Arun-Agnes@users.noreply.github.com>
2023-10-19 14:49:20 +0530

    Add files via upload

---
+++ storage/framework/views/9f1dc9a251cf9a4f67c0914fd16daa9d.php
@@ -0,0 +1,2 @@
+<input type="checkbox" <?php echo $attributes->merge(['class' => 'rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500']); ?>>
+<?php /**PATH C:\Users\aruna\OneDrive\Desktop\Arun_Laravel\Products_page\resources\views/components/checkbox.blade.php ENDPATH**/ ?>
